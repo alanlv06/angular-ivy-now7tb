1f6daee9cef5908c6c9197d08f453329c8ac8f8a
alanlv06 <alanlv06@gmail.com>
2021-09-10 16:39:28 +0200

    1 file modified

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrls: ['..component.css']
+  styleUrls: ['./appareil.component.css']
 })
 export class AppareilComponent implements OnInit {
   appareilName: string = 'Machine à laver';
