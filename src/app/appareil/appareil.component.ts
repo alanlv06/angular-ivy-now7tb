c73a7e557ca1d496829aa7a50b7737c45220c3a7
alanlv06 <alanlv06@gmail.com>
2021-09-10 16:57:31 +0200

    4 files modified

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrls: ['./appareil.component.css']
+  styleUrls: ['./appareil.component.scss']
 })
 export class AppareilComponent implements OnInit {
   appareilName: string = 'Machine à laver';
+  appareilStatus: string = 'éteint';
 
   constructor() {}
 
   ngOnInit() {}
+
+  getStatus() {
+    return this.appareilStatus;
+  }
 }
